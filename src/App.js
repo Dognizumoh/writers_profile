a96b2057c597dd3d043703d6f1cb46befe986e19
Dognizumoh <idafolo16@gmail.com>
2022-03-23 13:00:49 +0000

    added useState

--- src/App.js
+++ src/App.js
@@ -1,51 +1,38 @@
 import ProfileCard from "./components/ProfileCard";
 
-import React, { Component } from 'react'
+import React, {useState} from 'react';
 
- class App extends Component {
-constructor(){
-  super();
-
-this.handleClick =this.handleClick.bind(this);
-
-this.state ={
-  writers:{
-    loading: false,
-    list:[]
-  }
-
-};
-
-}
-handleClick(){
-this.setState({
-  writers:{
-    loading:true
-  }
-
+ function App() {
+const [writers,setWriters]=useState({
+  loading: false,
+  list:[]
 });
 
-setTimeout(async()=>{
+const handleClick = ()=>{
+setWriters((previousState)=>({
+  ...previousState,
+  loading:true
+
+}))
+setTimeout(async ()=>{
   let resp = await fetch("/writers.json");
   let result = await resp.json();
 
-  this.setState({
-    writers:{
-      loading:false,
-      list: result
-    }
-  });
-},3500);
 
-}
+setWriters((previousState)=>({
+  ...previousState,
+  loading:false,
+  list:result
+}));
+}, 2500)
+ };
+  
 
 
-  render() {
-  const {
-     writers:{loading, list}
-  }= this.state;
+  
 
-  if(loading){
+
+  if( writers.loading){
     return(
       <div>
       <h1>Writers Profile</h1>
@@ -65,14 +52,14 @@ setTimeout(async()=>{
     <div>
     <h1>Writers Profile</h1>
      <div className="container">
-     { list.length === 0 ? (
+     { writers.list.length === 0 ? (
        <div className="card action">
        <p>Oops....no writer profile found</p>
-       <button className="actionBtn" onClick={this.handleClick}>Get Writers</button>
+       <button className="actionBtn" onClick={handleClick}>Get Writers</button>
        </div>
 
      ):(
-     list.map((writer) =>(
+     writers.list.map((writer) =>(
        <ProfileCard key={writer.id} writer={writer}/>
 
      ))
@@ -84,7 +71,8 @@ setTimeout(async()=>{
   
   </div>
     )
-  }
-}
+
+  }     
+
 
 export default App
